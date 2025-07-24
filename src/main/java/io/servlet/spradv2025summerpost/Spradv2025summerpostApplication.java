20e5a034c9a30f663ed1b3d13309f8d6880006c6
jwoon0606 <jwoon0606@gmail.com>
2025-07-24 10:43:52 +0900

    Create Project

---
+++ src/main/java/io/servlet/spradv2025summerpost/Spradv2025summerpostApplication.java
@@ -0,0 +1,13 @@
+package io.servlet.spradv2025summerpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spradv2025summerpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spradv2025summerpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/servlet/spradv2025summerpost/Spradv2025summerpostApplicationTests.java
@@ -0,0 +1,13 @@
+package io.servlet.spradv2025summerpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spradv2025summerpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
